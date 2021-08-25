950e56e20ec06953bef5127a02ad36626b7b4965
Pierre CORBEL <pierre.corbel@payfit.com>
2021-08-25 22:16:01 +0200

    feat(worker): 🔊 add boinc-client logs to worker

--- worker/entrypoint.sh
+++ worker/entrypoint.sh
@@ -8,9 +8,12 @@ while true; do
   service boinc-client status | grep "Status of BOINC core client: running." > /dev/null 2>&1
   RESULT=$?
   if [ ${RESULT} -eq 0 ]; then
-    sleep 60
+    break
   else
     echo "BOINC core client is not started yet..."
     sleep 5
   fi
 done
+
+echo "Showing logs..."
+tail -f /var/lib/boinc-client/stdoutdae.txt
